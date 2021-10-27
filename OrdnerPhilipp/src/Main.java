2b3fbd55a99e8b61239d20766a3d54f806dc3cc7
pgerhardy <pgerhardy@ITEM-S112894.emea.msad.sopra>
2021-10-27 11:37:01 +0200

    Code Änderung -Philipp

--- OrdnerPhilipp/src/Main.java
+++ OrdnerPhilipp/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Das ist ein toller Code");
+		System.out.println("Das ist eine Änderung");
 	}
 
 }
