0e64b1bb6a1e315c97cb0c723976e91d13568809
Philipp <>
2021-11-10 12:22:00 +0100

    Aufgaben Damaris

--- OrdnerPhilipp/Aufgaben_Damaris_Geometrie/src/com/company/Main.java
+++ OrdnerPhilipp/Aufgaben_Damaris_Geometrie/src/com/company/Main.java
@@ -3,6 +3,37 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS KREIS
+        Kreis kreis = new Kreis(13);
+        System.out.println("Kreis");
+        System.out.println("- Radius: " + kreis.getRadius());
+        System.out.println("- Umfang: " + kreis.getUmfang());
+        System.out.println("- Flächeninhalt: " + kreis.getFlaecheninhalt());
+        kreis.setFlaeche(30);
+        System.out.println("- Radius (Abhängig von dem Flächeninhalt 30): " + kreis.getRadius());
+        kreis.setUmfang(15);
+        System.out.println("- Radius (Abhängig von dem Umfang 15): " + kreis.getRadius());
+        System.out.println(" ");
+
+        // RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK RECHTECK
+        Rechteck rechteck = new Rechteck(9,6);
+        rechteck.breiteVergroessern(3);
+        rechteck.breiteVerkleinern(4);
+        //Rechteck rechteck = new Rechteck(1);
+        System.out.println("Rechteck");
+        System.out.println("- Breite: " + rechteck.getBreite() + " und Länge: " + rechteck.getLaenge());
+        System.out.println("- Lange Seite: " + rechteck.getLangeSeite() + " und kurze Seite: " + rechteck.getKurzeSeite());
+        System.out.println("- Umfang: " + rechteck.getUmfang());
+        System.out.println("- Flächeninhalt: " + rechteck.getFlaecheninhalt());
+        System.out.println("- Diagonale: " + rechteck.getDiagonale());
+        System.out.println(" ");
+
+        // KREISTABELLE KREISTABELLE KREISTABELLE KREISTABELLE KREISTABELLE KREISTABELLE KREISTABELLE
+        kreis.setRadius(5);
+        for(int i = 0; i < 31; i++) {
+            System.out.println("Radius   Umfang    Fläche");
+            kreis.setRadius((5+5*i));
+            System.out.println(kreis.getRadius() + "    " + kreis.getUmfang() + "    " + kreis.getFlaecheninhalt());
+        }
     }
 }
--- OrdnerPhilipp/Aufgaben_Damaris_Geometrie/src/com/company/Rechteck.java
+++ OrdnerPhilipp/Aufgaben_Damaris_Geometrie/src/com/company/Rechteck.java
@@ -1,2 +1,89 @@
-package com.company;public class Rechteck {
+package com.company;
+
+public class Rechteck {
+    //Attribute
+    private double laenge;
+    private double breite;
+
+    //Konstruktor
+    public Rechteck(double laenge, double breite) {
+        this.laenge = laenge;
+        this.breite = breite;
+    }
+    public Rechteck(double Seite) {
+        this.laenge = Seite;
+        this.breite = Seite;
+    }
+
+    //Getter und Setter
+    public double getLaenge() {
+        return laenge;
+    }
+
+    public void setLaenge(double laenge) {
+        this.laenge = laenge;
+    }
+
+    public double getBreite() {
+        return breite;
+    }
+
+    public void setBreite(double breite) {
+        this.breite = breite;
+    }
+    //Setter Seiten
+    public void setSeiten(double laenge, double breite) {
+        this.laenge = laenge;
+        this.breite = breite;
+    }
+    //Getter Umfang
+    public double getUmfang(){
+        return 2*breite+2*laenge;
+    }
+
+    //Getter Flächeninhalt
+    public double getFlaecheninhalt() {
+        return breite*laenge;
+    }
+
+    //Getter Diagonale
+    public double getDiagonale() {
+        return Math.floor((Math.sqrt(laenge*laenge+breite*breite))*100)/100;
+    }
+
+    //Getter Lange Seite
+    public double getLangeSeite() {
+        if(breite>laenge) {
+            return breite;
+        }
+        else {
+            return laenge;
+        }
+    }
+    //Getter Kurze Seite
+    public double getKurzeSeite() {
+        if(breite<laenge) {
+            return breite;
+        }
+        else {
+            return laenge;
+        }
+    }
+    /*
+        ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG
+        ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG
+        ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG ERWEITERUNG
+     */
+    public void laengeVergroessern(double diff) {
+        laenge += diff;
+    }
+    public void breiteVergroessern(double diff) {
+        breite += diff;
+    }
+    public void laengeVerkleinern(double diff) {
+        laenge -= diff;
+    }
+    public void breiteVerkleinern(double diff) {
+        breite -= diff;
+    }
 }
